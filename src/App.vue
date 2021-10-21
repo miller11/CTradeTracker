78f411565aab79d1c099e4775e23d25829222bb2
Ross Miller <ross-h-miller@uiowa.edu>
2021-10-20 20:58:32 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <div>
+        <b-navbar toggleable="sm" type="dark" variant="dark">
+          <b-navbar-brand href="#">TradeTracker</b-navbar-brand>
+
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+          <b-collapse id="nav-collapse" is-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+              <b-nav-item-dropdown v-if="authState === 'signedin' && user" right>
+                <!-- Using 'button-content' slot -->
+                <template #button-content>
+                  <em>{{ user.attributes.email }}</em>
+                </template>
+                <b-dropdown-item @click="appSignOut()">Sign Out</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-navbar>
+      </div>
+    </div>
+
+
+
+    <amplify-authenticator username-alias="email"></amplify-authenticator>
+    <button type="button" @click="getGraph('bec75c28-d95a-52a9-8edf-d0947a133eda')">Call API</button>
+
+    <div id="graph"></div>
+
+
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {onAuthUIStateChange} from '@aws-amplify/ui-components'
+import { Auth } from 'aws-amplify';
+import axios from 'axios'
+import Plotly from 'plotly.js-dist'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'AuthStateApp',
+  created() {
+    this.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData;
+    })
+  },
+  data() {
+    return {
+      user: undefined,
+      authState: undefined,
+      unsubscribeAuth: undefined
+    }
+  },
+  methods: {
+    getGraph(accountId) {
+      const token = this.user.signInUserSession.idToken.jwtToken
+      const requestData = {
+        headers: {
+          Authorization: token,
+          "Access-Control-Allow-Origin": "*"
+        }
+      }
+
+      axios.get('https://yzn99f7f90.execute-api.us-east-1.amazonaws.com/Stage/trade-graph/' + accountId, requestData)
+          .then(response => {
+                const figure = JSON.parse(response.data.message)
+                console.log(figure)
+
+                Plotly.newPlot('graph', figure.data, figure.layout);
+              }
+          )
+    },
+    appSignOut: async function() {
+      try {
+        await Auth.signOut();
+        this.user = undefined;
+      } catch (error) {
+        console.log('error signing out: ', error);
+      }
+    }
+  },
+  beforeDestroy() {
+    this.unsubscribeAuth();
   }
 }
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import "@aws-amplify/ui-vue";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
